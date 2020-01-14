d5e3780fe91ff6a57203e1e1fc70d3e33121bf58
TR <18410078707@163.com>
2020-01-14 10:52:46 +0800

    第一次提交发行版本

---
+++ libaray/src/main/java/com/ekeguan/libaray/LogUtils.java
@@ -0,0 +1,12 @@
+package com.ekeguan.libaray;
+
+import android.util.Log;
+
+public class LogUtils {
+
+    private static final String TAG = "LogUtils";
+
+    public static void i(String log){
+        Log.i(TAG, log);
+    }
+}
